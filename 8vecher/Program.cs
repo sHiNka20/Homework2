ab6747f5f49dab658687cae3fe9165aaae6d8cb9
sHiNka20 <123722238+sHiNka20@users.noreply.github.com>
2023-09-09 00:40:04 +0300

    Add files via upload

--- 8vecher/Program.cs
+++ 8vecher/Program.cs
@@ -2,18 +2,18 @@
 
 Random rnd = new Random();
 
-int chet = 0;
+int a = 0;
+
 Console.Write("[");
-for(int i = 0; i < arr.Length;i++)
+for (int i = 0;i < arr.Length; i++)
 {
-    arr[i] = rnd.Next(100, 1000);
+    arr[i] = rnd.Next(-1000, 1001);
     Console.Write(arr[i]);
     Console.Write(", ");
 }
 Console.Write("] -> ");
-for(int i =0; i < arr.Length;i++)
+for (int i = 1; i < arr.Length; i += 2)
 {
-    if (arr[i] % 2 == 0)
-        chet++;
+    a = a + arr[i];
 }
-Console.WriteLine(chet);
+Console.WriteLine(a);
