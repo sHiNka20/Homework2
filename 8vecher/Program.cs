99cf8b751282eec020f657338d6ed255f2b5e41a
Стас <@DESKTOP-0OF5UJB>
2023-09-09 00:21:42 +0300

    Добавьте файлы проекта.

---
+++ 8vecher/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] arr = new int[4];
+
+Random rnd = new Random();
+
+int chet = 0;
+Console.Write("[");
+for(int i = 0; i < arr.Length;i++)
+{
+    arr[i] = rnd.Next(100, 1000);
+    Console.Write(arr[i]);
+    Console.Write(", ");
+}
+Console.Write("] -> ");
+for(int i =0; i < arr.Length;i++)
+{
+    if (arr[i] % 2 == 0)
+        chet++;
+}
+Console.WriteLine(chet);
